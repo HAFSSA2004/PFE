5793bc551a709e798297bdd0ff65c4802edccbfc
Nada <amranin772@gmail.com>
2025-03-17 00:01:39 +0100

    updt

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { FaEnvelope, FaLock } from "react-icons/fa";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from "react-router-dom";
 
 //import "@fontsource/inter"; // Defaults to 400 (normal)
 
